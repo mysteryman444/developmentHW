aeaca576f7a90112f3b1ace54366416e4e530698
mysteryman444 <115044022+mysteryman444@users.noreply.github.com>
2022-12-03 20:01:44 -0500

    Update bakeryItem.js

--- src/bakeryItem.js
+++ src/bakeryItem.js
@@ -5,7 +5,7 @@ import './bootsrap.css'
 const bakeryItem = ({ props }) => {
     return (
         <div>
-            <img class="card-img-top" src={props.img} alt={props.name} />
+            <img class="card-img-top" src={process.env.PUBLIC_URL+props.image} alt={props.name} />
             <h5 className="card-title">{props.name}&nbsp;&nbsp;${props.price}</h5>
             <h6>Vegan:<b>{(props.vegan) ? 'Yes' : 'No'}</b></h6>
             <h6>Gluten-Free:<b>{(props.gfStatus) ? 'Yes' : 'No'}</b></h6>
