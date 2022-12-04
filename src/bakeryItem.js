09f087523641651d03cb1a09cfc6f90616b233ff
mysteryman444 <115044022+mysteryman444@users.noreply.github.com>
2022-12-03 19:43:02 -0500

    Update bakeryItem.js

--- src/bakeryItem.js
+++ src/bakeryItem.js
@@ -6,7 +6,7 @@ const bakeryItem = ({ props }) => {
     return (
         <div>
             <img class="card-img-top" src={props.img} alt={props.name} />
-            <h5 className="card-title">{props.name}&nbsp;&nbsp;{props.price}</h5>
+            <h5 className="card-title">{props.name}&nbsp;&nbsp;${props.price}</h5>
             <h6>Vegan:<b>{(props.vegan) ? 'Yes' : 'No'}</b></h6>
             <h6>Gluten-Free:<b>{(props.gfStatus) ? 'Yes' : 'No'}</b></h6>
 
